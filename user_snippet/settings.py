68748696ed2c2cfb2e8e09e4e578d31009f16fee
Vasil <vrangelov85@abv.bg>
2023-08-07 16:53:29 +0300

    added drf_spectacular additional settings

--- user_snippet/settings.py
+++ user_snippet/settings.py
@@ -95,3 +95,11 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Testing Stuff",
+}
